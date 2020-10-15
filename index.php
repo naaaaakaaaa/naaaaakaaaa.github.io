33af324a726a86369b2963eb952d15eaa4359baf
naaaaakaaaa <67790079+naaaaakaaaa@users.noreply.github.com>
2020-10-15 22:36:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,67 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-$change = '';
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
 
-echo '店員：お会計は120円です。' . PHP_EOL;
-echo '自分：';
-$money = (int)trim(fgets(STDIN));
+<body>
+  <div class="header">
+    <div class="header-left">Progate</div>
+    <div class="header-right">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li class="selected">お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
 
-if ($money === 120) {
-  echo 'ちょうど頂戴いたします。ありがとうございました。' . PHP_EOL;
-} elseif ($change = $money - 120) {
-  echo $change . '円お返しいたします。ありがとうございました。' . PHP_EOL;
-}
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form method="post" action="sent.php">
+        <div class="form-item">名前</div>
+        <input type="text" name="name">
+
+        <div class="form-item">年齢</div>
+        <select name="age">
+          <option value="未選択">選択してください</option>
+          <!-- for文を用いて6歳から100歳までをoptionで選べるようにしましょう -->
+          <?php
+
+          ?>
+        </select>
+
+        <div class="form-item">お問い合わせの種類</div>
+        <?php
+        $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+        ?>
+        <!-- この下にselectタグを書いていきましょう -->
+
+
+        <div class="form-item">内容</div>
+        <textarea name="body"></textarea>
+
+        <input type="submit" value="送信">
+      </form>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+    </div>
+  </div>
+</body>
+
+</html>
